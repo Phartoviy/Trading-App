1db6d1934902314494cdec08cad92ffd68ec1668
Phartoviy <uran0215@mail.ru>
2023-05-25 22:10:20 +0300

    Create gui window and connection database

---
+++ manager.cpp
@@ -0,0 +1,65 @@
+﻿
+#include "manager.h"
+#include "./ui_manager.h"
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+#include <QtSql/QSqlRecord>
+#include <QDebug>
+#include <QTableWidgetItem>
+
+Manager::Manager(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Manager)
+{
+    ui->setupUi(this);
+    ui->pushButton->setFlat(true);
+    ui->pushButton->setText("Balance*");
+
+    ui->tableWidget->setRowCount(30);
+
+    ui->nameLabel->setText("Name: Pavel Dv.");
+
+
+    QSqlDatabase dbase = QSqlDatabase::addDatabase("QSQLITE");
+    dbase.setDatabaseName("trades.db");
+    QString str = "SELECT * FROM securities";
+    QString title;
+    int price, dividend, quantity;
+    if (dbase.open()){
+        qDebug() << "Super openned!";
+        QSqlQuery query;
+        bool b = query.exec(str);
+        QSqlRecord rec = query.record();
+        int numberRow = 0;
+        while(query.next())
+        {
+            title = query.value(rec.indexOf("title")).toString();
+            price = query.value(rec.indexOf("price")).toInt();
+            dividend = query.value(rec.indexOf("dividend")).toInt();
+            quantity = query.value(rec.indexOf("quantity")).toInt();
+            ui->tableWidget->setItem(numberRow,0,new QTableWidgetItem(title));
+            ui->tableWidget->setItem(numberRow,1,new QTableWidgetItem(QString::number(price)));
+            ui->tableWidget->setItem(numberRow,2,new QTableWidgetItem(QString::number(dividend)));
+            ui->tableWidget->setItem(numberRow++,3,new QTableWidgetItem(QString::number(quantity)));
+        }
+
+    }
+    connect(ui->pushButton,SIGNAL(clicked()),this,SLOT(balanceSlot()));
+
+}
+
+Manager::~Manager()
+{
+    delete ui;
+}
+
+
+void Manager::balanceSlot()
+{
+    (flagBalance)?flagBalance = false:flagBalance = true;
+    if (flagBalance)
+        ui->pushButton->setText(QString::number(balance));
+    else
+        ui->pushButton->setText("Balance*");
+}
+
---
+++ manager.h
@@ -0,0 +1,29 @@
+﻿
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include <QMainWindow>
+
+typedef int money;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Manager; }
+QT_END_NAMESPACE
+
+class Manager : public QMainWindow
+
+{
+    Q_OBJECT
+
+public:
+    Manager(QWidget *parent = nullptr);
+    ~Manager();
+private slots:
+    void balanceSlot();
+private:
+    Ui::Manager *ui;
+    bool flagBalance = false;
+    money balance = 0;
+};
+
+#endif // MANAGER_H
